d740e863c73782f7d782a164f54489ff4fef44ad
MarsonShine <ms27946@163.com>
2018-04-12 00:44:56 +0800

    fixed：内部类闭包功能demo

---
+++ InnerClasses/src/innerclasses/Incrementable.java
@@ -0,0 +1,61 @@
+package innerclasses;
+
+import static ms.util.Println.*;
+
+public interface Incrementable {
+    void increment();
+}
+
+class Callee1 implements Incrementable {
+    private int i = 0;
+
+    @Override
+    public void increment() {
+        i++;
+        print(i);
+    }
+}
+
+class MyIncrement {
+    public void increment() {
+        print("Other operation");
+    }
+
+    static void f(MyIncrement mi) {
+        mi.increment();
+    }
+}
+
+class Callee2 extends MyIncrement {
+    private int i = 0;
+
+    public void increment() {
+        super.increment();
+        i++;
+        print(i);
+    }
+
+    private class Closure implements Incrementable {
+        @Override
+        public void increment() {
+            //指定外部类方法，否则你会无限递归
+            Callee2.this.increment();
+        }
+    }
+
+    Incrementable getCallbackReference() {
+        return new Closure();
+    }
+}
+
+class Caller {
+    private Incrementable callbackReference;
+
+    Caller(Incrementable cbh) {
+        callbackReference = cbh;
+    }
+
+    void go() {
+        callbackReference.increment();
+    }
+}
