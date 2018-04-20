7ebe3c391639144dca0bc630097b144e8e6dd41c
MarsonShine <ms27946@163.com>
2018-04-21 02:23:14 +0800

    fixed：完善

--- Book/src/strings/StringVsStringBuilder.java
+++ Book/src/strings/StringVsStringBuilder.java
@@ -3,24 +3,26 @@ package strings;
 import performance.Stopwatch;
 
 public class StringVsStringBuilder {
-    protected static final String INIT_STRING = "abcdefghijklmn";
+    protected static final String INIT_STRING = "132";
 
     public static void main(String[] args) {
         var sw = new Stopwatch();
+        sw.restart();
+        var sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append(INIT_STRING);
+        }
+        sw.end();
+        System.out.println("StringBuilder 运行时间：" + sw.ElapsedMilliseconds() + " ms");
+
         sw.start();
         var str = "";
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             str += INIT_STRING;
         }
         sw.end();
         System.out.println("String + 运行时间：" + sw.ElapsedMilliseconds() + " ms");
 
-        sw.restart();
-        var sb = new StringBuilder();
-        for (int i = 0; i < 20000000; i++) {
-            sb.append(INIT_STRING);
-        }
-        sw.end();
-        System.out.println("StringBuilder 运行时间：" + sw.ElapsedMilliseconds() + " ms");
+
     }
 }
